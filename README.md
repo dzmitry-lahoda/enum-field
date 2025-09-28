68a58066646b3e1f1c4673bc81e61a957c605a89
dzmitry-lahoda <dzmitry@lahoda.pro>
2025-09-28 11:47:49 +0100

    actual docs

--- README.md
+++ README.md
@@ -18,7 +18,7 @@ allow to
 ```rust
 use AB::*;
 use XY::*;
-let product = Product { bx: "bx".to_string(), .. }; 
+let product = Product { b_x: "b_x".to_string(), a_y: 2, .. }; 
 let item = field_use!(product . B _ X);
 assert_eq!(item, "bx");
 
@@ -37,8 +37,13 @@ I assume it is possible with declarative macro, but prove of impossiblity also g
 
 ## Results
 
-To have heterogenous fileds types need macro export from macro, which is hard on stable.
+TLDR; Non intrisuvie macro with manual list of variants for few variants enums was done. 
 
-On top of that, cannot expand `list` of values and then tt muncher, says tt muncher is less deep.
+I feel like this handling such case can be done in Haskell, may be via TypeClasses.
 
-TLRD; Wait for https://github.com/rust-lang/rust/issues/83527 and try again. 
+In Rust, to have heterogenous fileds types need macro export from macro.
+
+In macro cannot expand `list` of values and then tt muncher, says tt muncher is less deep
+nor cannot avoid manual enumartion of units(without being intrusive).
+
+Wait for https://github.com/rust-lang/rust/issues/83527 and try again to improve macro. 
--- src/main.rs
+++ src/main.rs
@@ -35,6 +35,43 @@ macro_rules! enum_field_match {
 }
 
 #[macro_export]
+macro_rules! test2 {
+    ($a0: ident) => {
+        test2!{#internal $a0 $}
+    };
+    (#internal $a0:ident $dollar:tt) => {
+        macro_rules! test3 {
+            ($dollar a1:ident) => {
+                $a0
+            };
+        }
+    };
+}
+
+#[macro_export]
+macro_rules! test1 {
+    ($a0:ident) => {
+
+         $a0
+    };
+//     ($a0:ident; b0:ident) => {
+//         $a0
+//    };
+//     ([$a0:ident]) => {
+//         test1!($a0)
+//     };
+//     ([$a0:ident], [$b0:ident]) => {
+//         $a0 + $b0
+//     };
+//     ([$a0:ident, $a1:ident], ) => {
+//         match 
+//     };
+//     ([$a0:ident, $a1:ident, $a2:ident], $rest:tt) => {
+        
+//     };
+}
+
+#[macro_export]
 macro_rules! enum_filed_use {
     ($self:ident . $ab:ident _ $xy:ident) => {
         ::paste::paste! {
@@ -49,7 +86,7 @@ macro_rules! enum_filed_use {
 }
 
 // in theory could make macro arounds enums to get all variants, but seems to intrusive for now (expecially if some enum is very generic for use case)
-enum_field_match!(get_match, Product, AB, XY, &str; A, B; X, Y);
+// enum_field_match!(get_match, Product, AB, XY, &str; A, B; X, Y);
 
 #[derive(Clone, Copy)]
 pub enum AB {
@@ -71,26 +108,31 @@ pub struct Product {
     pub b_y: u8,
 }
 
+
+
+
 fn main() {
     use AB::*;
     use XY::*;
+    test2!(A);
+    test3!(B);
     let mut product = Product {
         a_x: "a_x".to_string(),
-        a_y: "a_y".to_string(),
+        a_y: 1,
         b_x: "b_x".to_string(),
-        b_y: "b_y".to_string(),
+        b_y: 2,
     };
 
     {
         assert_eq!(product.a_x, enum_filed_use!(product.A _ X).as_str());
     }
-    {
-        let a_x = product.a_x.clone();
-        assert_eq!(a_x, enum_filed_use!(mut product.A _ X).as_str());
-        let setter = enum_filed_use!(mut product.A _ X);
-        *setter = "new_a_x".to_string();
-    }
-    let matcher = get_match!(AB::A, XY::X);
-    assert_eq!(product.a_x, matcher(&product));
-    println!("{product:?}");
+    // {
+    //     let a_x = product.a_x.clone();
+    //     assert_eq!(a_x, enum_filed_use!(mut product.A _ X).as_str());
+    //     let setter = enum_filed_use!(mut product.A _ X);
+    //     *setter = "new_a_x".to_string();
+    // }
+    // let matcher = get_match!(AB::A, XY::X);
+    // assert_eq!(product.a_x, matcher(&product));
+    // println!("{product:?}");
 }
